0d45dfe3c78d5ed57c54f9a80d74e202387b0168
Abhishek Kumar <akroy2456@gmail.com>
2022-10-06 19:45:53 +0530

    make it better

--- app.js
+++ app.js
@@ -87,9 +87,11 @@ app.get("/contact",(req,res)=>{
     res.render("contact");
 });
 
+let port =process.env.PORT;
+if(port==null||port==""){
+  port=3000;
+}
 
-
-app.listen(3000, function () {
-
-    console.log("Server is running at port 3000");
+app.listen(port, function() {
+  console.log("Server started on port 3000");
 });
